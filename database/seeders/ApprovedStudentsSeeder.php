4a50644e90e828f7d46376b1a05e871135510fd5
Froyd <froydcarbajosa14@gmail.com>
2025-12-08 03:00:38 +0800

    Add approved BSIT 2-B students to seeder
    Added multiple ApprovedStudent::create entries for BSIT 2nd year, section B students in the ApprovedStudentsSeeder. This expands the seed data to include all approved students for this section, useful for testing and initial database population.

--- database/seeders/ApprovedStudentsSeeder.php
+++ database/seeders/ApprovedStudentsSeeder.php
@@ -29,5 +29,332 @@ class ApprovedStudentsSeeder extends Seeder
             'year_level' => 2,
             'section' => 'B',
         ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00819',
+            'name' => 'ABUTON, CARL JUDI LABAO',
+            'email' => 'abuton@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00802',
+            'name' => 'ACEDO, TRIZTAN MARCOS',
+            'email' => 'acedo@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00637',
+            'name' => 'AGAG, RUZEL JHON YGONIA',
+            'email' => 'agag@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00813',
+            'name' => 'AGMOHOL, MELVIN JR. BASCO',
+            'email' => 'agmohol@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00767',
+            'name' => 'AMANDOG, KIRBY GIAN COMIDA',
+            'email' => 'amandog@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00579',
+            'name' => 'AMOGUIS, CLEMM BATUCAN',
+            'email' => 'amoguis@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00393',
+            'name' => 'BALBERONA, NERELIE',
+            'email' => 'balberona@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00530',
+            'name' => 'BALDON, JOHN LORENS ABUDA',
+            'email' => 'baldon@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '202204-01714',
+            'name' => 'BASILLOTE, JONARD DIVINO',
+            'email' => 'basillote@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00558',
+            'name' => 'BATILONG, KIMBIE JARO',
+            'email' => 'batilong@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00684',
+            'name' => 'BAYACAG, GIAN CEDRICK',
+            'email' => 'bayacag@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00724',
+            'name' => 'CAPUNO, CHRISTIAN LACSON',
+            'email' => 'capuno@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00633',
+            'name' => 'CARBAJOSA, FROYD DUGOY',
+            'email' => 'carbajosa@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-02234',
+            'name' => 'DESIERTO, HENDREY CLARIDAD',
+            'email' => 'desierto@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00776',
+            'name' => 'EYAS, CHRISTIAN JAMES OGONG',
+            'email' => 'eyas@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00758',
+            'name' => 'FUENTES, KHINT JOSEPH OMPAD',
+            'email' => 'fuentes@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00927',
+            'name' => 'GABAYE, JON ARCELLE GEMOTA',
+            'email' => 'gabaye@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00737',
+            'name' => 'GAVE, ZAIRANICOLE SOMBRIO',
+            'email' => 'gave@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00731',
+            'name' => 'GEQUILAN, ELVIE NARIZ',
+            'email' => 'gequilan@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00388',
+            'name' => 'GOLOSINO, FELAURA VIVIEN',
+            'email' => 'golosino@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        // ---- SECOND COLUMN ----
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00647',
+            'name' => 'MANTE, CYRIL MARK FLORES',
+            'email' => 'mante@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00591',
+            'name' => 'MENDOZA, JOHN PAUL REDORME',
+            'email' => 'mendoza@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00785',
+            'name' => 'OSAHITA, JOY ANNE LITERAL',
+            'email' => 'osahita@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00545',
+            'name' => 'PAGALAN, BRYL JAMES',
+            'email' => 'pagalan@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00585',
+            'name' => 'PANGAN, TJ LANADA',
+            'email' => 'pangan@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00849',
+            'name' => 'PARSAN, RUEL CLIANT',
+            'email' => 'parsan@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-01903',
+            'name' => 'PEREZ, SEVERINO PAMA',
+            'email' => 'perez@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00551',
+            'name' => 'QUINES, MONCH WALTER',
+            'email' => 'quines@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00797',
+            'name' => 'RABOR, FELIX VICTOR DAROCA',
+            'email' => 'rabor@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-02510',
+            'name' => 'RAMIREZ, EARL VINCENT REYES',
+            'email' => 'ramirez@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00832',
+            'name' => 'RODRIGUEZ, KARYL ROSE RAMA',
+            'email' => 'rodriguez@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2023-00535',
+            'name' => 'ROTA, JHON CARLO VILLARANTE',
+            'email' => 'rota@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00654',
+            'name' => 'SARIO, KRISHIA MAE',
+            'email' => 'sario@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00840',
+            'name' => 'TULISANA, DIXTER BAJENTING',
+            'email' => 'tulisana@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00665',
+            'name' => 'TUMAGAN, LIEZEL NERI',
+            'email' => 'tumagan@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
+        ApprovedStudent::create([
+            'student_id' => '2024-00699',
+            'name' => 'VILLAR, RAFAEL MAR LAMINTAC',
+            'email' => 'villar@gmail.com',
+            'course' => 'BSIT',
+            'year_level' => 2,
+            'section' => 'B',
+        ]);
+
     }
 }
